d15c034abfab393f6d52de8d278e7398f6a433ab
Lukas Mohs <ga47boq@mytum.de>
2018-04-07 14:41:39 +0200

    changed plots and interest analysis

--- script.R
+++ script.R
@@ -111,9 +111,9 @@ length(which(data$X2a == 1 & data$X1d != 3)) / numberNonCarDrivers # 0.514
 # Travel Distance
 mean(data$X.1c) # 23.82794
 median(data$X.1c) #18.1
-hist(data$X.1c.,xlab="Travel Kilometers",main="", breaks=50, col="red")
-hist(data$X.1c.2,add=T, col="green", breaks=50)
-legend("topright", c("Not Interested", "Interested"), fill=c("red", "green"))
+hist(data$X.1c.,xlab="Travel Kilometers",main="", breaks=50, col="gray")
+hist(data$X.1c.2,add=T, col="#f9af23", breaks=50)
+legend("topright", c("Not Interested", "Interested"), fill=c("gray", "#f9af23"))
 
 # Travel Distance on Travel Option
 plot(data$X.1c.,data$X1d, xlab="Travel Distance", ylab="Traevl Option")
@@ -123,8 +123,8 @@ abline(lm(X1d ~ X.1c., data=data),col="red")
 startHour = strptime("6:00:00",format='%H:%M:%S')
 endHour = strptime("22:00:00",format='%H:%M:%S')
 series <- seq(from = startHour, to = endHour, by = "hour")
-hist(data$X5b,"mins",xlab="Arrival and Departure Time",col=rgb(1,0,0,0.4), breaks=50, xlim=as.POSIXct(c(startHour,endHour)),xaxt='n', ann=FALSE)
-hist(data$X5a,"mins",xlab="Arrival Time",add=T, col=rgb(0,1,0,0.4), breaks=35, xaxt='n', ann=FALSE)
+hist(data$X5b,"mins",xlab="Arrival and Departure Time",col=rgb(.5,.5,.5,0.4), breaks=50, xlim=as.POSIXct(c(startHour,endHour)),xaxt='n', ann=FALSE)
+hist(data$X5a,"mins",xlab="Arrival Time",add=T, col=rgb(.98,.69,.14,0.6), breaks=35, xaxt='n', ann=FALSE)
 axis.POSIXct(1, at=c(series), labels=format(series, '%H:%M'))
 legend("topright", c("Arrival Time","Departure Time"), fill=c(rgb(0,1,0,0.4),rgb(1,0,0,0.4)),cex = 1.2)
 
@@ -150,32 +150,32 @@ axis(1,c(0.5:5.5),c("5 minutes","10 minutes","15 minutes","20 minutes","30 minut
 # Travel Distance on Willingsness to Pay
 hist(data$wppkm,xlab="Willingsness to Pay in Euro per Commute KM ",breaks=40,main="",xaxt='n')
 axis(1,c(seq(0, 1, 0.05)),c(seq(0, 1, 0.05)),ylab="bla")
-plot(data$X.1c.,data$X6a, xlab="Travel Distance", ylab="Willingsness to Pay for total Distance",xlim = c(0,90))
+plot(col=rgb(.98,.69,.14,1), data$X.1c.,data$X6a, xlab="Travel Distance", ylab="Willingsness to Pay for total Distance",xlim = c(0,90))
 wtplm = lm(X6a ~ X.1c., data=data) #linear model by fitting
 summary(wtplm)$sigma  # residual standard deviation:  2.343433
-abline(wtplm,col="red")
+abline(wtplm,col=rgb(.98,.69,.14,1))
 abline(0,0.3,col="blue")
 abline(0,0.6,col="green")
-legend("topright", c("Average Traveling Costs by Car (60 cents/km)","German Tax Refunds (30 cents/km)","Fitted Willingsness to Pay"), fill=c("green","blue", "red"),cex = 1.2)
+legend("topright", c("Average Traveling Costs by Car (0,6\u20ac/km)","German Tax Refunds (0,3\u20ac/km)","Fitted Willingsness to Pay"), fill=c("green","blue",rgb(.98,.69,.14,1)),cex = 1.2)
 mean(data$wppkm, na.rm=TRUE) # 0.1817779
 median(data$wppkm, na.rm=TRUE) # 0.1296794
 
 # Travel Distance on Expected Payout
-hist(data$eppkm,xlab="Expected Payout in Euro per Commute KM",breaks=40)
-plot(data$X.1c.,data$X7a, xlab="Travel Distance", ylab="Expected Payout in Euro for total Distance",xlim = c(0,120),ylim = c(0,10))
+hist(col="darkgray",data$eppkm,xlab="Expected Payout in Euro per Commute Kilometer",breaks=40)
+plot(col="darkgray", data$X.1c.,data$X7a, xlab="Travel Distance", ylab="Expected Payout in Euro for total Distance",xlim = c(0,120),ylim = c(0,10))
 eppklm = lm(X7a ~ X.1c., data=data)
 summary(eppklm)$sigma #1.066353
-abline(eppklm,col="red")
+abline(eppklm,col="darkgray")
 abline(0,0.3,col="blue")
 abline(0,0.6,col="green")
-legend("topright", c("Average Traveling Costs by Car (60 cents/km)","German Tax Refunds (30 cents/km)","Fitted Expected Payout"), fill=c("green","blue", "red"),cex = 1.2)
+legend("topright", c("Average Traveling Costs by Car (0,6\u20ac/km)","German Tax Refunds (0,3\u20ac/km)","Fitted Expected Payout"), fill=c("green","blue", "darkgray"),cex = 1.2)
 mean(data$eppkm, na.rm=TRUE) # 0.1262702
 median(data$eppkm, na.rm=TRUE) # 0.09353147
 
 # Detour Distance on Expected Payout
-hist(data$epdkm,xlab="Expected Payout in Euro per Detour KM",breaks=40, main="",xaxt='n')
+hist(col="gray", data$epdkm,xlab="Expected Payout in Euro per Detour Kilometer",breaks=40, main="",xaxt='n')
 axis(1,c(seq(-0.025, 2, 0.1)),c(seq(0, 2, 0.1)),ylab="bla")
-plot(data$X.1c.,data$X7b, xlab="Travel Distance", ylab="Expected Payout in Euro per 10 Detour KM")
+plot(data$X.1c.,data$X7b, xlab="Travel Distance", ylab="Expected Payout in Euro per 10 Detour Kilometer")
 abline(lm(X7b ~ X.1c., data=data),col="red")
 mean(data$epdkm, na.rm=TRUE) # 0.4479167
 median(data$epdkm, na.rm=TRUE) # 0.5
@@ -183,14 +183,14 @@ median(data$epdkm, na.rm=TRUE) # 0.5
 
 
 # Combined Drive & Passenger Prices
-plot(data$X.1c.,data$X7a, xlab="Travel Distance", ylab="Expected Payout in Euro for total Distance",xlim = c(0,120),ylim = c(0,10),col="red")
-points(data$X6a,col=rgb(0.54,0.17,0.89,0.4))
+plot(data$X.1c.,data$X7a, xlab="Travel Distance", ylab="Expected Payout in Euro for total Distance",xlim = c(0,120),ylim = c(0,10),col="darkgray")
+points(data$X6a,col=rgb(.98,.69,.14,1))
 axis(4,c(seq(0, 10, 2)),c(seq(0, 10, 2)),ylab="bla",col=rgb(0.54,0.17,0.89,0.4))
-mtext("Willingness to Pay", side=4, col=rgb(0.54,0.17,0.89,0.4), line = -2)
-legend("topright", c("Average Traveling Costs by Car (60 cents/km)","German Tax Refunds (30 cents/km)","Fitted Expected Payout","Fitted Willingness to Pay"), fill=c("green","blue", "red",rgb(0.54,0.17,0.89,0.4)),cex = 1.2)
+mtext("Willingness to Pay", side=4, col=rgb(.98,.69,.14,1), line = -2)
+legend("topright", c("Average Traveling Costs by Car (60 cents/km)","German Tax Refunds (30 cents/km)","Fitted Expected Payout","Fitted Willingness to Pay"), fill=c("green","blue", "gray",rgb(.98,.69,.14,1)),cex = 1.2)
 eppklm = lm(X7a ~ X.1c., data=data)
 summary(eppklm)$sigma #1.066353
-abline(eppklm,col="red")
+abline(eppklm,col="darkgray")
 abline(wtplm,col=	rgb(0.54,0.17,0.89,0.4))
 abline(0,0.3,col="blue")
 abline(0,0.6,col="green")
@@ -204,7 +204,7 @@ legend("topright", 95, legend=c("1: No Need"  ,"2: Not interested financially","
                                 "5: Public transport is more convenient and especially more sustainable","6: Traffic jams","7: Environment"), cex=.95)
 
 
-hist(data$X9b,xlab="Reasons for not being interested in ride-sharing",xaxt='n',ylim=c(0, 50), main="", breaks = 9)
+hist(data$X9b,xlab="Reasons for not being interested in ride-sharing",xaxt='n',ylim=c(0, 50), main="", breaks = 9, col="gray")
 axis(1,c(1.5:7.5),c("1", "2", "3", "4", "5", "6", "7"))
 legend("topright", 95, legend=c("1: Public transport is more convenient"  ,"2: Not interested financially","3: No Need","4: Loss of privacy",
                                 "5: Public transport is more convenient and especially more sustainable","6: Traffic jams","7: Environment"), cex=.95)
